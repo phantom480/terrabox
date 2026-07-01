651499380988064c25030c6b622653259500569f
mohd aarif <73133970+phantom480@users.noreply.github.com>
2026-07-01 17:30:28 +0530

    Update terraform.tf
    Removed unused provider configurations for random, tls, and local.

--- terrabox/terraform.tf
+++ terrabox/terraform.tf
@@ -5,15 +5,4 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 6.0"
-    }
-    random = {
-      source = "hashicorp/random"
-    }
-    tls = {
-      source = "hashicorp/tls"
-    }
-    local = {
-      source = "hashicorp/local"
-    }
-  }
-}
+   
